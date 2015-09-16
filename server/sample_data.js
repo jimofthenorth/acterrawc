7ff646a9bcb6e20413467cb99e0ad06534c488e2
jimofthenorth <james.larrenaga@gmail.com>
2015-09-16 11:26:03 -0700

    [style](style.css, wc_dates.html, sample.data.js) style changes to dates, update mock data

--- server/sample_data.js
+++ server/sample_data.js
@@ -3,43 +3,43 @@ if(Stations.find().count() === 0) {
     stationName: 'M1',
     lat: 37.404611,
     lng: -122.157279,
-    waterBody: 'Matadero',
+    waterBody: 'Matadero Creek',
     samples: [
       {
         'Date': '11/21/2013',
-        'Water Body': 'Matadero',
+        'Water Body': 'Matadero Creek',
         'Station Name': 'M1',
-        'Air Temperature': 16.5,
+        'Air Temperature (Degrees C)': 16.5,
         'pH': 7.61,
-        'Turbidity': 12,
-        'Conductivity': 1316
+        'Turbidity (NTU/JTU)': 12,
+        'Conductivity (µS)': 1316
       },
       {
         'Date': '1/30/2014',
-        'Water Body': 'Matadero',
+        'Water Body': 'Matadero Creek',
         'Station Name': 'M1',
-        'Air Temperature': 17.0,
+        'Air Temperature (Degrees C)': 17.0,
         'pH': 7.97,
-        'Turbidity': '<5',
-        'Conductivity': 2060
+        'Turbidity (NTU/JTU)': '<5',
+        'Conductivity (µS)': 2060
       },
       {
         'Date': '4/18/2014',
-        'Water Body': 'Matadero',
+        'Water Body': 'Matadero Creek',
         'Station Name': 'M1',
-        'Air Temperature': 20.0,
+        'Air Temperature (Degrees C)': 20.0,
         'pH': 7.5,
-        'Turbidity': 0,
-        'Conductivity': '2.2 mS'
+        'Turbidity (NTU/JTU)': 0,
+        'Conductivity (µS)': '2.2 mS'
       },
       {
         'Date': '5/16/2014',
-        'Water Body': 'Matadero',
+        'Water Body': 'Matadero Creek',
         'Station Name': 'M1',
-        'Air Temperature': 24.0,
+        'Air Temperature (Degrees C)': 24.0,
         'pH': 7.92,
-        'Turbidity': 1.53,
-        'Conductivity': 1935
+        'Turbidity (NTU/JTU)': 1.53,
+        'Conductivity (µS)': 1935
       }
     ]
   });
@@ -48,25 +48,25 @@ if(Stations.find().count() === 0) {
     stationName: 'M2',
     lat: 37.409526,
     lng: -122.138392,
-    waterBody: 'Matadero',
+    waterBody: 'Matadero Creek',
     samples: [
       {
         'Date': '11/21/2013',
-        'Water Body': 'Matadero',
+        'Water Body': 'Matadero Creek',
         'Station Name': 'M2',
-        'Air Temperature': 'No Data Available',
+        'Air Temperature (Degrees C)': 'No Data Available',
         'pH': 7.75,
-        'Turbidity': 20,
-        'Conductivity': 911
+        'Turbidity (NTU/JTU)': 20,
+        'Conductivity (µS)': 911
       },
       {
         'Date': '1/30/2014',
-        'Water Body': 'Matadero',
+        'Water Body': 'Matadero Creek',
         'Station Name': 'M2',
-        'Air Temperature': 15.0,
+        'Air Temperature (Degrees C)': 15.0,
         'pH': 7.82,
-        'Turbidity': '<5',
-        'Conductivity': 1200
+        'Turbidity (NTU/JTU)': '<5',
+        'Conductivity (µS)': 1200
       }
     ]
   });
@@ -75,43 +75,43 @@ if(Stations.find().count() === 0) {
     stationName: 'S1',
     lat: 37.414394,
     lng: -122.189715,
-    waterBody: 'San_Francisquito',
+    waterBody: 'San Francisquito Creek',
     samples: [
       {
         'Date': '11/21/2013',
-        'Water Body': 'San_Francisquito',
+        'Water Body': 'San Francisquito Creek',
         'Station Name': 'S1',
-        'Air Temperature': 'No Data Available',
+        'Air Temperature (Degrees C)': 'No Data Available',
         'pH': 7.49,
-        'Turbidity': 17.5,
-        'Conductivity': 967
+        'Turbidity (NTU/JTU)': 17.5,
+        'Conductivity (µS)': 967
       },
       {
         'Date': '1/25/2014',
-        'Water Body': 'San_Francisquito',
+        'Water Body': 'San Francisquito Creek',
         'Station Name': 'S1',
-        'Air Temperature': 9.0,
+        'Air Temperature (Degrees C)': 9.0,
         'pH': 7.82,
-        'Turbidity': 2,
-        'Conductivity': 1330
+        'Turbidity (NTU/JTU)': 2,
+        'Conductivity (µS)': 1330
       },
       {
         'Date': '2/22/2014',
-        'Water Body': 'San_Francisquito',
+        'Water Body': 'San Francisquito Creek',
         'Station Name': 'S1',
-        'Air Temperature': 15.0,
+        'Air Temperature (Degrees C)': 15.0,
         'pH': 7.98,
-        'Turbidity': 3,
-        'Conductivity': 1020
+        'Turbidity (NTU/JTU)': 3,
+        'Conductivity (µS)': 1020
       },
       {
         'Date': '3/22/2014',
-        'Water Body': 'San_Francisquito',
+        'Water Body': 'San Francisquito Creek',
         'Station Name': 'S1',
-        'Air Temperature': 16.0,
+        'Air Temperature (Degrees C)': 16.0,
         'pH': 8,
-        'Turbidity': 3,
-        'Conductivity': 9.8
+        'Turbidity (NTU/JTU)': 3,
+        'Conductivity (µS)': 9.8
       }
     ]
   });
@@ -120,34 +120,34 @@ if(Stations.find().count() === 0) {
     stationName: 'S2',
     lat: 37.41364,
     lng: -122.192693,
-    waterBody: 'San_Francisquito',
+    waterBody: 'San Francisquito Creek',
     samples: [
       {
         'Date': '11/21/2014',
-        'Water Body': 'San_Francisquito',
+        'Water Body': 'San Francisquito Creek',
         'Station Name': 'S2',
-        'Air Temperature': 10,
+        'Air Temperature (Degrees C)': 10,
         'pH': 7.73,
-        'Turbidity': 2,
-        'Conductivity': 1266
+        'Turbidity (NTU/JTU)': 2,
+        'Conductivity (µS)': 1266
       },
       {
         'Date': '1/25/2014',
-        'Water Body': 'San_Francisquito',
+        'Water Body': 'San Francisquito Creek',
         'Station Name': 'S2',
-        'Air Temperature': 15,
+        'Air Temperature (Degrees C)': 15,
         'pH': 8.3,
-        'Turbidity': 5,
-        'Conductivity': 19.8
+        'Turbidity (NTU/JTU)': 5,
+        'Conductivity (µS)': 19.8
       },
       {
         'Date': '3/22/2014',
-        'Water Body': 'San_Francisquito',
+        'Water Body': 'San Francisquito Creek',
         'Station Name': 'S2',
-        'Air Temperature': 16.0,
+        'Air Temperature (Degrees C)': 16.0,
         'pH': 8.23,
-        'Turbidity': 1.18,
-        'Conductivity': 1500
+        'Turbidity (NTU/JTU)': 1.18,
+        'Conductivity (µS)': 1500
       }
     ]
   })
